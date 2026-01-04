5999af524f755f35e54d9f51c8418ec9293272bb
Matthew Popp <poppmat03@gmail.com>
2026-01-03 22:37:01 -0600

    Add empty portfolio component

--- project-portfolio/src/app/app.routes.ts
+++ project-portfolio/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Portfolio } from './components/portfolio/portfolio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Portfolio }
+];
