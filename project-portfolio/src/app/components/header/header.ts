7bf0ccd2c5a3062858b5f3c7294cf2cf2f9aafbf
Matthew Popp <poppmat03@gmail.com>
2026-01-04 23:11:44 -0600

    Add header component

---
+++ project-portfolio/src/app/components/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-header',
+  imports: [ButtonModule],
+  templateUrl: './header.html',
+})
+export class Header {
+
+}
