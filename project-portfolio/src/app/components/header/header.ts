6dfaa00ff2ecb8aa0c3a2bce2242c91b1f79d0d1
Matthew Popp <poppmat03@gmail.com>
2026-01-05 19:52:47 -0600

    Add resume sidebar

--- project-portfolio/src/app/components/header/header.ts
+++ project-portfolio/src/app/components/header/header.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
+import { Resume } from "../resume/resume";
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonModule],
+  imports: [ButtonModule, Resume, DrawerModule],
   templateUrl: './header.html',
 })
 export class Header {
-
+  protected resumeVisible = signal(false);
 }
