0fc7386ed64572b72435582266f48c7bd909f1ca
Matthew Popp <poppmat03@gmail.com>
2026-01-06 18:46:32 -0600

    Add icon images and enum

---
+++ project-portfolio/src/app/enums/icons.ts
@@ -0,0 +1,12 @@
+export enum icons {
+    angular = "/assets/icons/angular.webp",
+    excel = "/assets/icons/excel.webp",
+    python = "/assets/icons/python.webp",
+    tableau = "/assets/icons/tableau.webp",
+    aws = "/assets/icons/aws.webp",
+    sap = "/assets/icons/sap.webp",
+    sqlServer = "/assets/icons/sql-server.webp",
+    mongodb = "/assets/icons/mongo.webp",
+    mysql = "/assets/icons/mysql.webp",
+    github = "/assets/icons/github.webp"
+}
